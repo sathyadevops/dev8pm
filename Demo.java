87fd4bb6337811c0ff9bf208883f71053da20480
sathyadevops <sathyadevops1@gmail.com>
2021-06-14 14:53:08 +0000

    code

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+  public static void main(....)
+  {
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+  }
+}
